9db3f69dac52101b717f481e898d0abc78ed9941
Mike Dowd <mdowd@salesforce.com>
2019-05-02 15:08:50 -0700

    one

---
+++ src/main/java/com/example/hack2019/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.hack2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
