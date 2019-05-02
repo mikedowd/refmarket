5d02f8ecca733ba30a2a6f199c057bd59ab08a57
Mike Dowd <mdowd@salesforce.com>
2019-05-02 16:48:27 -0700

    add referral request controller

---
+++ src/main/java/com/example/hack2019/ReferralRequestController.java
@@ -0,0 +1,33 @@
+package com.example.hack2019;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReferralRequestController {
+
+	@RequestMapping(method = RequestMethod.POST, value = "/referralrequest")
+	public void processReferralRequest(@RequestBody ReferralRequest request) throws Exception{
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value="/referralrequest")
+	public List<ReferralRequest> getReferralRequest(){
+		
+		ReferralRequest rr = new ReferralRequest("what", "94110", "exit", new Date(2019, 3, 2), new Date(2019, 3, 6), "network", "gender", (short) 18);
+		ReferralRequest rr2 = new ReferralRequest("what", "94110", "exit", new Date(2019, 3, 2), new Date(2019, 3, 6), "network", "gender", (short) 18);
+
+		ArrayList<ReferralRequest> rrs = new ArrayList<ReferralRequest>();
+		
+		rrs.add(rr);
+		rrs.add(rr2);
+		
+		return new ArrayList<ReferralRequest>();
+	}
+}
