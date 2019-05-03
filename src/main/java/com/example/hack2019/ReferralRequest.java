54455be176d797fb5037e120330af7f9e6a80875
Mike Dowd <mdowd@salesforce.com>
2019-05-02 21:43:43 -0700

    add jdbc to ref req controller

--- src/main/java/com/example/hack2019/ReferralRequest.java
+++ src/main/java/com/example/hack2019/ReferralRequest.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class ReferralRequest {
 
-	private final long id; 
-	private final String orgURL;
+	private final int id; 
+	private final String patient_url;
 	private final String locationZip;
 	private final String specialty;
 	private final Date startDate;
@@ -14,11 +14,11 @@ public class ReferralRequest {
 	private final String gender;
 	private final short age;
 
-	public ReferralRequest(long id, String orgURL, String locationZip, String specialty, Date startDate, Date endDate,
+	public ReferralRequest(int id, String patient_url, String locationZip, String specialty, Date startDate, Date endDate,
 			String network, String gender, short age) {
 		super();
 		this.id = id;
-		this.orgURL = orgURL;
+		this.patient_url = patient_url;
 		this.locationZip = locationZip;
 		this.specialty = specialty;
 		this.startDate = startDate;
@@ -28,12 +28,12 @@ public class ReferralRequest {
 		this.age = age;
 	}
 	
-	public long getId() {
+	public int getId() {
 		return id;
 	}
 	
-	public String getOrgURL() {
-		return orgURL;
+	public String getPatientURL() {
+		return patient_url;
 	}
 	public String getLocationZip() {
 		return locationZip;
--- src/main/java/com/example/hack2019/ReferralRequestController.java
+++ src/main/java/com/example/hack2019/ReferralRequestController.java
@@ -1,38 +1,98 @@
 package com.example.hack2019;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReferralRequestController {
 
-	@RequestMapping(method = RequestMethod.POST, value = "/referralrequest")
+	@Autowired
+	private DataSource dataSource;
+	  
+	@RequestMapping(method = RequestMethod.POST, value = "/referralrequests")
 	public void processReferralRequest(@RequestBody ReferralRequest request) throws Exception{
-		
+		try (Connection connection = dataSource.getConnection()) {
+			PreparedStatement ps = connection.prepareStatement(
+					"insert into treatmentrequests(patient_url, location, specialty, startdate, enddate, network, gender, age) "
+					+ "values (?, ?, ?, ?, ?, ?, ?, ?)");
+			ps.setString(1, request.getPatientURL());
+			ps.setString(2, request.getLocationZip());
+			ps.setString(3, request.getSpecialty());
+			ps.setDate(4, (java.sql.Date) request.getStartDate());
+			ps.setDate(5, (java.sql.Date) request.getEndDate());
+			ps.setString(6, request.getNetwork());
+			ps.setString(7, request.getGender());
+			ps.setInt(8, request.getAge());
+			
+			ps.execute();
+			
+		} catch (Exception e) {
+		}
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value="/referralrequest")
-	public List<ReferralRequest> getReferralRequest(){
-		
-		ReferralRequest rr = new ReferralRequest(1, "what", "94110", "exit", new Date(2019, 3, 2), new Date(2019, 3, 6), "network", "gender", (short) 18);
-		ReferralRequest rr2 = new ReferralRequest(2, "what", "94110", "exit", new Date(2019, 3, 2), new Date(2019, 3, 6), "network", "gender", (short) 18);
+	@RequestMapping(method = RequestMethod.GET, value="/referralrequests")
+	public List<ReferralRequest> getReferralRequests(@RequestParam(value="zipcode") String zipcode,
+			@RequestParam(value="specialty") String specialty,
+			@RequestParam(value="availabledate") @DateTimeFormat(pattern="yyyy-MM-dd") Date availabledate,
+			@RequestParam(value="network") String network,
+			@RequestParam(value="gender") String gender,
+			@RequestParam(value="ageRange") String ageRange
+			){
+		ArrayList<ReferralRequest> requests = new ArrayList<ReferralRequest>();
+		try (Connection connection = dataSource.getConnection()) {
+			
+			String[] ages = ageRange.split(":");
+			
+			PreparedStatement ps = connection.prepareStatement(
+					"select id, patient_url, location, specialty, startdate, enddate, network, gender, age "
+					+ "from treatmentrequests "
+					+ "where location=? AND specialty=? AND network=? and gender=? and age between ? and ? and "
+					+ "? between startdate and enddate");
+			ps.setString(1, zipcode);
+			ps.setString(2, specialty);
+			ps.setString(3, network);
+			ps.setString(4, gender);
+			ps.setInt(5, Integer.parseInt(ages[0]));
+			ps.setInt(6, Integer.parseInt(ages[1]));
+			ps.setDate(7, (java.sql.Date) availabledate);
+			
+			ResultSet rs = ps.executeQuery();
 
-		ArrayList<ReferralRequest> rrs = new ArrayList<ReferralRequest>();
+//			public ReferralRequest(int id, String orgURL, String locationZip, String specialty, Date startDate, Date endDate,
+//					String network, String gender, short age) {
+
+			
+			while (rs.next()) {
+				ReferralRequest request = new ReferralRequest(
+						rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDate(5), rs.getDate(6), 
+						rs.getString(7), rs.getString(8), rs.getShort(9));
+
+				requests.add(request);
+			}
+			
+		} catch (Exception e) {
+		}
 		
-		rrs.add(rr);
-		rrs.add(rr2);
-		
-		return rrs;
+		return requests;
 	}
 	
-	@RequestMapping(method = RequestMethod.POST, value="/referralrequest/accept")
-	public void acceptReferralRequest() throws Exception{
+	@RequestMapping(method = RequestMethod.POST, value="/referralrequests/{id}/accept")
+	public void acceptReferralRequest(@PathVariable String id) throws Exception{
 		
 	}
 }
